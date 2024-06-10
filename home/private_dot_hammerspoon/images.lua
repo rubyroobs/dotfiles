c307744c277582dded0e6ee6f07aa1a6973d8630
Ruby Nealon <ruby@ruby.sh>
2024-06-10 15:09:48 +0900

    tidying up a little, ranger

--- home/private_dot_hammerspoon/images.lua
+++
@@ -1,6 +0,0 @@
-return {
-  swap = hs.image.imageFromPath("images/swap.png"),
-  stack = hs.image.imageFromPath("images/stack.png"),
-  split = hs.image.imageFromPath("images/split.png"),
-  warp = hs.image.imageFromPath("images/warp.png"),
-}
