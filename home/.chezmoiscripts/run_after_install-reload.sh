ba6a20e0752b346966a79bb44e493c67cc546132
Ruby Nealon <rnealon@gitlab.com>
2024-07-21 18:51:52 +0900

    templated colors and font, vs code

--- home/.chezmoiscripts/run_after_install-reload.sh
+++ home/.chezmoiscripts/run_after_install-reload.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# VS Code
+if [ -d "$HOME/Library/Application Support" ]; then
+    mkdir -p "$HOME/Library/Application Support/Code/User"
+    cp "$HOME/.config/Code/User/settings.json" "$HOME/Library/Application Support/Code/User/"
+    cp "$HOME/.config/Code/User/keybindings.json" "$HOME/Library/Application Support/Code/User/"
+    cp -R "$HOME/.config/Code/User/snippets" "$HOME/Library/Application Support/Code/User/"
+fi
+
 if command -v fc-cache > /dev/null 2>&1; then
     fc-cache
 fi
